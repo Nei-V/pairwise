32b00bca77e611df4d3e6c9e230b2b51fa5dd8b3
Ion Varsescu <19718355+Nei-V@users.noreply.github.com>
2018-04-30 00:40:14 +0300

    final version
    Signed-off-by: Ion Varsescu <19718355+Nei-V@users.noreply.github.com>

--- lessonslearned.md
+++ lessonslearned.md
@@ -1,3 +1,12 @@
-1. turning string into array
+1. turning string with values separated by comma into array
 I used this resource:
 https://stackoverflow.com/questions/13272406/convert-string-with-commas-to-array
+
+the first step is to split the string by the commas and then using maps to turn each item into a number
+
+2. in this challenge you need a reduce inside the loop.
+
+3. using reduce - moslty learned from MDN - usage of the currentIndex (the third argument of reduce) - this is the index of the current item. 
+very important ! ! ! - if you give a start value for the reduce, it will run from index 0 of the array each time, so in this excerciese you have to check that the current index is different than the accumulator index. this is important (especialy) beacuse we are using reduce inside the loop. if you don't use start value for accumulator, the index of the current starts from 1, not 0. 
+
+4. you can do a the reduce inside a foreach, but I finnaly understood the problem with the index only when I used the for loop. 
--- pairwise.js
+++ pairwise.js
@@ -1,11 +1,44 @@
 function pairwise(arr, arg) {
-    let originalString=document.getElementById("originalArrayString").value;
-    let originalArray=originalString.split(",").map(Number);
-    let sum=document.getElementById("target").value;
-    console.log("original array",originalArray, "and the target sum is,",sum);
-    let result=0;
-    document.getElementById("result").innerHTML=result;
-  return arg;
-}
+  let originalString = document.getElementById("originalArrayString").value;
+  let originalArray = originalString.split(",").map(Number);
+  let sum = document.getElementById("target").value;
+  console.log("original array", originalArray, "and the target sum is,", sum);
+  let result = [];
+  let sumOfSumIndices = 0;
+  let sumIndices = 0;
+  let itemIndex = 0;
 
-//document.getElementById("result").innerHTML=pairwise([1,4,2,3,0,5], 7);
+  for (let i = 0; i < originalArray.length - 1; i++) {
+    if (originalArray[i] !== undefined) {
+      result = originalArray.reduce((accumulator, current, currentIndex) => {
+        let sumIndices = 0;
+        console.log("originalArray before if", originalArray);
+        console.log("current before if", current, "currentIndex", currentIndex, "sum is", sum, "sumIndices", sumIndices, "accumulator", accumulator);
+        if ((accumulator !== undefined) && (current !== undefined) && (currentIndex !== i) && (itemIndex !== (originalArray.length - 1)) && (accumulator + current == sum)) {
+          console.log("original length is in if", originalArray.length, "place of acumulator", i, "place of current in if", currentIndex);
+          console.log("sumIndices in if", sumIndices);
+          sumIndices = i + currentIndex;
+          sumOfSumIndices += sumIndices;
+          accumulator = undefined;
+          current = undefined;
+          originalArray[currentIndex] = undefined;
+          sumIndices = 0;
+        }
+        else {
+          sumIndices = 0;
+        };
+        originalArray[i] = undefined;
+        console.log("sumIndices", sumIndices);
+        console.log("accumulator is", accumulator, "current is", current);
+
+        return accumulator;
+      }, originalArray[i]);
+    };
+
+  };
+  console.log("sumofallindeces", sumOfSumIndices);
+
+  document.getElementById("result").innerHTML = sumOfSumIndices;
+  return sumOfSumIndices;
+};
+
